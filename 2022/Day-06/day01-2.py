02f299af17747bb926cbc908bf1d366ed09fd61e
Jake Charman <jake@jakecharman.co.uk>
2022-12-06 09:19:18 +0000

    Completed day 6

---
+++ 2022/Day-06/day01-2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+input = open('input.txt', 'r').readline()
+
+i = 0
+different = []
+while(len(different) < 13):
+    if(input[i] not in different):
+        different.append(input[i])
+    else:
+        different = []
+    i += 1
+
+print(i - 1)
